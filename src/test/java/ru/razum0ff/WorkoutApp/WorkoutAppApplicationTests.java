c6769a2d70e7d4c37744e52ac557b5b80414355c
09NINE90 <razumov.aleksei101@yandex.ru>
2024-10-04 17:57:47 +0500

    init

---
+++ src/test/java/ru/razum0ff/WorkoutApp/WorkoutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.razum0ff.WorkoutApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
